f003a5f058b94729d37d728636da8449799b31e3
san <sango7199@gmail.com>
2023-06-21 17:50:57 +0900

    6월21일 (수)

--- src/Baekjoon/step03/_08_11022.java
+++ src/Baekjoon/step03/_08_11022.java
@@ -3,8 +3,8 @@ package Baekjoon.step03;
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class _07_11021 {
-    // A+B - 7
+public class _08_11022 {
+    // A+B - 8
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -14,7 +14,9 @@ public class _07_11021 {
         int T = Integer.parseInt(br.readLine());
         for (int i = 1; i <= T; i++) {
             st = new StringTokenizer(br.readLine());
-            bw.write("Case #" + i + ": " +  (Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+            bw.write("Case #" + i + ": " + num1 + " + " + num2 + " = " + (num1+num2) + "\n");
         }
         br.close();
         bw.flush();

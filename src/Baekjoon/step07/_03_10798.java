1fead676d6106bfc0be7689f1be8eb1992292c29
san <sango7199@gmail.com>
2023-07-19 23:48:41 +0900

    7월 19일 (수)

--- src/Baekjoon/step07/_03_10798.java
+++ src/Baekjoon/step07/_03_10798.java
@@ -1,8 +1,26 @@
 package Baekjoon.step07;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class _03_10798 {
     // 세로 읽기
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        char[][] arr = new char[5][15];
+        for (int i = 0; i < 5; i++) { // 입력
+            str = br.readLine();
+            for (int j = 0; j < str.length(); j++) {
+                arr[i][j] = str.charAt(j);
+            }
+        }
+        for (int i = 0; i < arr[0].length; i++) { // 출력
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j][i] != '\0') System.out.print(arr[j][i]);
+            }
+        }
+        br.close();
     }
 }
---
+++ src/Baekjoon/step07/_04_2563.java
@@ -0,0 +1,8 @@
+package Baekjoon.step07;
+
+public class _04_2563 {
+    // 색종이
+    public static void main(String[] args) {
+        
+    }
+}

b745ec4fdd53d5a3200c5bbffd4226a41867ccd6
san <sango7199@gmail.com>
2023-10-16 22:36:12 +0900

    이사 끝나고 재개 10월 16일 (월)

---
+++ src/Baekjoon/step10/_04_15894.java
@@ -0,0 +1,14 @@
+package Baekjoon.step10;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class _04_15894 {
+    // 수학은 체육과목입니다
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long N = sc.nextInt();
+        sc.close();
+        System.out.print(4 * N);
+    }
+}
